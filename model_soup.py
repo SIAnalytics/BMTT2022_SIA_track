3cd977375deb02c22d425e066098e9d3469bb0e8
seominseok <33244972+seominseok0429@users.noreply.github.com>
2022-05-27 00:00:49 +0900

    Update model_soup.py

--- model_soup.py
+++ model_soup.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 
-ckpt = torch.load('72.6.pth.tar', map_location='cpu')
-ckpt2 = torch.load('73.6.pth.tar', map_location='cpu')
+ckpt = torch.load('69.6.pth.tar', map_location='cpu') # fine-tune model1
+ckpt2 = torch.load('70.1.pth.tar', map_location='cpu') # fine-tune model2
 
 for k, v in ckpt["model"].items():
     f = ckpt2["model"][k]*0.5 + v*0.5
