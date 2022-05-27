614259db02ffe60aadaeb0475bdee71c0c6ca4e8
seominseok <33244972+seominseok0429@users.noreply.github.com>
2022-05-27 11:39:44 +0900

    Update yolox_x_find_tune.py

--- exps/example/mot/yolox_x_find_tune.py
+++ exps/example/mot/yolox_x_find_tune.py
@@ -20,14 +20,14 @@ class Exp(MyExp):
         self.input_size = (800, 1440)
         self.test_size = (800, 1440)
         self.random_size = (18, 32)
-        self.max_epoch = 80
-        self.print_interval = 20
-        self.eval_interval = 5
+        self.max_epoch = 5
+        self.print_interval = 1
+        self.eval_interval = 1
         self.test_conf = 0.1
         self.nmsthre = 0.7
-        self.no_aug_epochs = 10
-        self.basic_lr_per_img = 0.001 / 64.0
-        self.warmup_epochs = 1
+        self.no_aug_epochs = 0
+        self.basic_lr_per_img = 0.0001 / 64.0
+        self.warmup_epochs = 0
 
     def get_data_loader(self, batch_size, is_distributed, no_aug=False):
         from yolox.data import (
