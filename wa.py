54f3b23f4f019082ef5abcb6aa9562a56d5b5b7a
seominseok <33244972+seominseok0429@users.noreply.github.com>
2022-05-31 17:36:44 +0900

    Update wa.py

--- wa.py
+++ wa.py
@@ -13,7 +13,7 @@ ckpt = torch.load(args.path1, map_location='cpu') # fine-tune model1
 ckpt2 = torch.load(args.path2, map_location='cpu') # fine-tune model2
 
 for k, v in ckpt["model"].items():
-    f = ckpt2["model"][k]*0.08 + v*0.92
+    f = ckpt2["model"][k]*0.4 + v*0.6
     ckpt2["model"][k] = f
 
 torch.save(ckpt2, 'soup.pth.tar')
