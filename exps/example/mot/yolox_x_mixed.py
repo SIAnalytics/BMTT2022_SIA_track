7c62ec1d29c94d0deb52a694927eda533ace3c55
seominseok <33244972+seominseok0429@users.noreply.github.com>
2022-05-31 17:43:38 +0900

    Update yolox_x_mixed.py

--- exps/example/mot/yolox_x_mixed.py
+++ exps/example/mot/yolox_x_mixed.py
@@ -27,7 +27,7 @@ class Exp(MyExp):
         self.nmsthre = 0.7
         self.no_aug_epochs = 10
         self.ema = True
-        self.basic_lr_per_img = 0.001 / 64.0
+        self.basic_lr_per_img = 0.00025 / 64.0
         self.warmup_epochs = 0
 
     def get_data_loader(self, batch_size, is_distributed, no_aug=False):
